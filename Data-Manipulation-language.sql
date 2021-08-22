8d3112b5dd40a7321abe67b1374d9c6b918f6bd9
Your Name <you@example.com>
2021-08-22 23:57:17 +0200

    first commit

---
+++ Data-Manipulation-language.sql
@@ -0,0 +1,11 @@
+ALTER TABLE Product ADD Category VARCHAR2(20);
+ALTER TABLE Orders ADD OrderDate DATE DEFAULT SYSDATE ;
+
+INSERT INTO Customer VALUES('C01','ALI',71321009);
+INSERT INTO Customer VALUES('C02','ASMA',77345823);
+
+INSERT INTO Product VALUES('P01','Samsung Galaxy S20','Smartphone',3299);
+INSERT INTO Product VALUES('P02','ASUS Notebook','PC',4599);
+
+INSERT INTO Orders (Customer_id,Product_id,Quantity,Total_amount) VALUES('C01','P02',2,9198);
+INSERT INTO Orders VALUES('C02','P01','28/05/2020',1,3299);
